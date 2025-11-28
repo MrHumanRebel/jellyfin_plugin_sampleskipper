5cca209b79d23fe96aba91a47715455535a03e9d
Székely Dániel <80947401+MrHumanRebel@users.noreply.github.com>
2025-11-28 09:12:39 +0100

    Refactor Plugin constructor for Jellyfin 10.11.3
    Updated the Plugin constructor to remove IXmlSerializer parameter due to changes in Jellyfin 10.11.3.

--- Jellyfin.Plugin.SampleSkipper/Plugin.cs
+++ Jellyfin.Plugin.SampleSkipper/Plugin.cs
@@ -7,7 +7,6 @@ namespace Jellyfin.Plugin.SampleSkipper
 {
     public class Plugin : BasePlugin
     {
-        // Unique GUID for the plugin
         public override Guid Id => Guid.Parse("a4f5d6e7-1b2c-4d3e-5f6g-7h8i9j0k1l2m");
 
         public override string Name => "Sample Skipper";
@@ -16,8 +15,10 @@ namespace Jellyfin.Plugin.SampleSkipper
 
         public static Plugin Instance { get; private set; }
 
+        // FIX: The BasePlugin constructor in Jellyfin 10.11.3 no longer requires IXmlSerializer.
+        // We only pass IApplicationPaths to the base class.
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) 
-            : base(applicationPaths, xmlSerializer)
+            : base(applicationPaths) 
         {
             Instance = this;
         }
